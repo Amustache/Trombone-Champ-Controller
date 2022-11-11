5655b8d3158aedd7b884df27cd81d3d9b4f22ea5
Stache <stache@stache.cat>
2022-11-11 20:59:15 +0100

    Update

--- ultrasound/ultrasound.ino
+++ ultrasound/ultrasound.ino
@@ -3,13 +3,14 @@
 #define echoPin 2
 #define trigPin 3
 #define butPin 4
-#define MOV_DELTA 25
+#define MOV_DELTA 50
 #define ERROR_CONTROL 0.5
 
 float prev_distance;
 float distance;
 float diff_distance;
 int buttonState;
+int mov;
 
 float get_distance(int trig, int echo) {
   float dist;
@@ -22,7 +23,9 @@ float get_distance(int trig, int echo) {
   delayMicroseconds(10);
   digitalWrite(trig, LOW);
   // Reads the echoPin, returns the sound wave travel time in microseconds
+  noInterrupts();
   dur = pulseIn(echo, HIGH);
+  interrupts();
   // Calculating the distance
   dist = dur * 0.034 / 2; // Speed of sound wave divided by 2 (go and back)
 
@@ -43,18 +46,27 @@ void loop() {
   distance = get_distance(trigPin, echoPin);
   
   Serial.print("ButtonState: ");
-  Serial.println(buttonState);
+  if (buttonState == 1) {
+    Serial.println("pressed");
+  } else {
+    Serial.println("Not pressed");
+  }
 
-  if (buttonState == 1) {Mouse.press();} else {Mouse.release();}
-  
-  Serial.print("previousState - currentState: ");
-  Serial.println(prev_distance - distance);
+  //if (buttonState == 1) {Mouse.press();} else {Mouse.release();}
 
   diff_distance = prev_distance - distance;
   // Ugly error control lol
   if ((diff_distance > 0.0 && diff_distance < ERROR_CONTROL) || (diff_distance < 0.0 && diff_distance > -ERROR_CONTROL)) {diff_distance = 0.0;}
+  mov = int(diff_distance * MOV_DELTA);
 
-  Mouse.move(0, diff_distance * MOV_DELTA, 0);
+  Serial.print("Current distance: ");
+  Serial.print(distance);
+  Serial.print(", difference: ");
+  Serial.print(diff_distance);
+  Serial.print(", effect: ");
+  Serial.println(mov);
+
+  Mouse.move(0, mov, 0);
 
   prev_distance = distance;
 
